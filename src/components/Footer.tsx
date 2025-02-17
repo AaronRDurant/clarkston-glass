e79ab1a8c8112bdaf7f8991c186a1f78ba45065a
Aaron Durant <aaron@aarondurant.com>
2025-02-16 22:19:13 -0500

    Set up global layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full p-4 bg-gray-900 text-white text-center">
+      © {new Date().getFullYear()} Clarkston Glass. All rights reserved.
+    </footer>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full p-4 bg-gray-800 text-white">
+      <div className="container mx-auto">Clarkston Glass</div>
+    </nav>
+  );
+}
