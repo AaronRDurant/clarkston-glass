4bc93e42f8939a7b1dead87f788310c6d09c610c
Aaron Durant <aaron@aarondurant.com>
2025-04-04 22:35:41 -0400

    Create contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ContactForm from "@/components/ContactForm";
+
+export default function ContactPage() {
+  return (
+    <div className="mt-32">
+      <ContactForm />
+    </div>
+  );
+}
