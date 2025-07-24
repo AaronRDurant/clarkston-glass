8649e36af332cd04268c93b7736219b8dff20612
Aaron Durant <aaron@aarondurant.com>
2025-07-23 21:11:29 -0400

    Add structured data using LocalBusiness scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,49 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-white text-[#2c2c2c] antialiased min-h-screen flex flex-col">
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              name: "Clarkston Glass",
+              image: "https://www.clarkstonglass.net/images/og/og-default.jpg",
+              "@id": "https://www.clarkstonglass.net",
+              url: "https://www.clarkstonglass.net",
+              telephone: "+1-248-625-5911",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "6577 Dixie Hwy",
+                addressLocality: "Clarkston",
+                addressRegion: "MI",
+                postalCode: "48346",
+                addressCountry: "US",
+              },
+              openingHoursSpecification: [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: [
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                  ],
+                  opens: "08:00",
+                  closes: "17:00",
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: "Saturday",
+                  opens: "08:00",
+                  closes: "12:00",
+                },
+              ],
+              sameAs: ["https://www.facebook.com/clarkstonglass"],
+            }),
+          }}
+        />
         <Navbar />
         <main className="flex-grow">{children}</main>
         <Footer />
